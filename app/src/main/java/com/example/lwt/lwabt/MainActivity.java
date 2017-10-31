56618edf373e4f4bd6477f3716d5586027b364d1
lwt <727141749@qq.com>
2017-10-31 13:29:49 +0800

    这是第一次提交的消息

--- app/src/main/java/com/example/lwt/lwabt/MainActivity.java
+++ app/src/main/java/com/example/lwt/lwabt/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //zhesadasdasdasdasd
         ///asdasdasdasdasdasdasd
+
+        //asdhuiahsdhaksdkj11111111111111111
     }
 }
